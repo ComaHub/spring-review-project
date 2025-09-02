872270348aad41c96ed49bfdda4a17562df78569
Coma <comasocean@gmail.com>
2025-09-02 17:38:33 +0900

    Create: project 생성

---
+++ src/main/java/dev/coma/review/SpringReviewProjectApplication.java
@@ -0,0 +1,13 @@
+package dev.coma.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReviewProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReviewProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/coma/review/SpringReviewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.coma.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReviewProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
